ebfac186b950a51552a1348e8ddc0efe6f8958a8
oylmz <oytunylmz@gmail.com>
2021-02-27 13:58:27 +0100

    fetch multiple symbols

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BINANCE_API_KEY = ''
-BINANCE_API_SECRET = ''
-start_time_str = 'January 01, 2021 GMT+2'
-end_time_str = 'February 01, 2021 GMT+2'
+start_time_str = '01-01-2021'
+end_time_str = '27-02-2021'
+candlestick_interval = '15m'
+symbols = ['BTCUSDT', 'ETHUSDT']
